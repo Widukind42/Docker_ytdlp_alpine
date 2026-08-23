cc62deea4206dc754004004ba16ab0552cc85a02
Widukind42 <Widukind42@proton.me>
2026-08-23 12:49:06 +0200

    Delete .github/scripts/update-package-inventory.sh

--- .github/scripts/update-package-inventory.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-OUTFILENAME="PackageInventory.md"
-THISPACKAGE="ghcr.io/widukind42/ytdlp_alpine"
-# Container-Infos in eine Datei schreiben
-echo "### Package Inventory (Stand: $(date +"%Y-%m-%d"))" > $OUTFILENAME
-echo "" >> $OUTFILENAME
-podman run --rm -v "$(pwd):/out:z" --entrypoint /bin/sh ghcr.io/widukind42/ytdlp_alpine /out/.github/scripts/show-container-releases.sh >> $OUTFILENAME
-echo "" >> $OUTFILENAME
